d0a6946edfcba93ec5fdcf0f9982b98faee363e8
Mo'men Mohamed <84360276+momen882001@users.noreply.github.com>
2024-04-22 00:26:30 +0200

    add PopUp component in shared

---
+++ client/src/core/components/pop-up/PopUp.tsx
@@ -0,0 +1,81 @@
+import { Box, Typography } from "@mui/material";
+import React from "react";
+import CloseRoundedIcon from "@mui/icons-material/CloseRounded";
+
+interface PopUpProps {
+  isDisplay: boolean;
+  setDisplayPopUp: React.Dispatch<React.SetStateAction<boolean>>;
+  title: string;
+  children: React.ReactNode;
+  width? : string
+}
+
+const PopUp = ({ isDisplay, setDisplayPopUp, title, children , width = "40vw" }: PopUpProps) => {
+  return (
+    <Box sx={{ display: isDisplay ? "block" : "none" }}>
+      <Box
+        sx={{
+          position: "fixed",
+          top: "0",
+          left: "0",
+          width: "100%",
+          height: "100%",
+          backgroundColor: "#000000aa",
+          zIndex: "1000",
+          filter: "blur(0.5px)",
+        }}
+        onClick={() => setDisplayPopUp(false)}
+      ></Box>
+      <Box
+        sx={{
+          padding: "2rem",
+          position: "absolute",
+          top: "50%",
+          left: "50%",
+          transform: "translate(-50%, -50%)",
+          width: width,
+          height: "fit-content",
+          zIndex: "1001",
+        }}
+      >
+        <Box
+          sx={{
+            position: "relative",
+            backgroundColor: "white",
+            width: "100%",
+            height: "100%",
+            borderRadius: "15px",
+          }}
+        >
+          <Box
+            sx={{
+              backgroundColor: "primary.dark",
+              display: "flex",
+              justifyContent: "space-between",
+              alignItems: "center",
+              padding: ".25rem 2rem",
+              borderTopLeftRadius: "15px",
+              borderTopRightRadius: "15px",
+            }}
+          >
+            <Typography sx={{ color: "white", fontWeight: "600" }}>
+              {title}
+            </Typography>
+            <CloseRoundedIcon
+              sx={{
+                color: "white",
+                margin: "0.5rem 0",
+                fontSize: "2rem",
+                cursor: "pointer",
+              }}
+              onClick={() => setDisplayPopUp(false)}
+            />
+          </Box>
+          <Box sx={{ padding: "2rem" }}>{children}</Box>
+        </Box>
+      </Box>
+    </Box>
+  );
+};
+
+export default PopUp;
