0a79b741d4721402065be851c13625b823d7e1e6
Abdelrhman Yasser <abdelrhman.abdelhakem00@eng-st.cu.edu.eg>
2024-05-02 12:07:22 +0300

    Doctor Ui

---
+++ doctor-client/src/modules/patients/components/response/DataCard.tsx
@@ -0,0 +1,16 @@
+import Box from '@mui/material/Box'
+import React from 'react'
+import Header from '../../../../core/components/Header'
+
+const DataCard = ({ header, children }: { header: string, children: any }) => {
+    return (
+        <Box sx={{ padding: '1rem', backgroundColor: 'rgb(32, 37, 45)', height:'100%' }}>
+            <Header title={header} dark />
+            <Box sx={{ mt: 1 }}>
+                {children}
+            </Box>
+        </Box>
+    )
+}
+
+export default DataCard
