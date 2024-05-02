037a0328c20704eec82b82269bc7e3bd23136812
Abdelrhman Yasser <abdelrhman.abdelhakem00@eng-st.cu.edu.eg>
2024-05-02 12:21:35 +0300

    Update DataCard.tsx

--- doctor-client/src/modules/patients/components/response/DataCard.tsx
+++ doctor-client/src/modules/patients/components/response/DataCard.tsx
@@ -4,7 +4,7 @@ import Header from '../../../../core/components/Header'
 
 const DataCard = ({ header, children }: { header: string, children: any }) => {
     return (
-        <Box sx={{ padding: '1rem', backgroundColor: 'rgb(32, 37, 45)', height:'100%' }}>
+        <Box sx={{ padding: '1rem', backgroundColor: 'rgb(32, 37, 45)', height: '100%', borderRadius: '5px' }}>
             <Header title={header} dark />
             <Box sx={{ mt: 1 }}>
                 {children}
